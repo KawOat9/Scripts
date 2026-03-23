07a0247dee5ea70fb727d0dcc249e5ba35ecf30d
@KawOat9 <boonseub85@gmail.com>
2026-03-23 15:35:58 +0700

    Add DarkReader.js

---
+++ Userscripts/DarkReader.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Dark Reader✨
+// @namespace   darkreader.org
+// @version     1.0.0
+// @description Dark Mode for every website
+// @author      KawOatツ
+// @match       *://*/*
+// @grant       GM_addStyle
+// @icon        https://raw.githubusercontent.com/darkreader/darkreader.github.io/master/images/darkreader-mascot.svg
+// @require https://cdn.jsdelivr.net/npm/darkreader/darkreader.min.js
+// @license MIT
+// ==/UserScript==
+
+// Enable when the system color scheme is dark.
+DarkReader.auto({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10
+});
