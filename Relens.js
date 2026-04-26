97c0059852bf3f622d44b79dbcca650b89307bf4
@KawOat9 <boonseub85@gmail.com>
2026-04-26 20:12:31 +0700

    Update Relens.js

--- Relens.js
+++ Relens.js
@@ -24,7 +24,7 @@ objc = {
 		"receipt_type": "Production",
 		"app_item_id": 944665061,
 		"receipt_creation_date": "2023-06-06 16:06:26 Etc/GMT",
-		"bundle_id": "com.risingcabbage.pro.camera.relensproforever",
+		"bundle_id": "com.risingcabbage.pro.camera.relensproforever2025",
 		"in_app": [{
 			"quantity": "1",
 			"purchase_date_ms": "1686065612000",
@@ -35,7 +35,7 @@ objc = {
 			"is_trial_period": "false",
 			"original_transaction_id": "490001314520000",
 			"purchase_date": "2023-06-06 16:06:06 Etc/GMT",
-			"product_id": "com.risingcabbage.pro.camera.relensproforever",
+			"product_id": "com.risingcabbage.pro.camera.relensproforever2025",
 			"original_purchase_date_pst": "2023-06-06 06:06:07 America/Los_Angeles",
 			"in_app_ownership_type": "PURCHASED",
 			"original_purchase_date_ms": "1686065613000",
@@ -69,7 +69,7 @@ objc = {
 		"is_trial_period": "false",
 		"original_transaction_id": "490001314520000",
 		"purchase_date": "2023-06-06 16:06:06 Etc/GMT",
-		"product_id": "com.risingcabbage.pro.camera.relensproforever",
+		"product_id": "com.risingcabbage.pro.camera.relensproforever2025",
 		"original_purchase_date_pst": "2023-06-06 06:06:07 America/Los_Angeles",
 		"in_app_ownership_type": "PURCHASED",
 		"original_purchase_date_ms": "1686065613000",
@@ -81,9 +81,9 @@ objc = {
 	"latest_receipt": "Crack",
 	"environment": "Production",
 	"pending_renewal_info": [{
-		"product_id": "com.risingcabbage.pro.camera.relensproforever",
+		"product_id": "com.risingcabbage.pro.camera.relensproforever2025",
 		"original_transaction_id": "490001314520000",
-		"auto_renew_product_id": "com.risingcabbage.pro.camera.relensproforever",
+		"auto_renew_product_id": "com.risingcabbage.pro.camera.relensproforever2025",
 		"auto_renew_status": "1"
 	}],
 	"warning": "仅供学习，禁止转载或售卖"
