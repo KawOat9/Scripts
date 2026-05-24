7febc5c0f72e958a65dc473b00bfc07d6c8b3b15
@KawOat9 <boonseub85@gmail.com>
2026-05-24 11:14:06 +0700

    Update BuyRevenueCat.js

--- BuyRevenueCat.js
+++ BuyRevenueCat.js
@@ -209,8 +209,7 @@ const mapping = {
   'Blackout/': ['premium','blackout_299_lt'],
   'Budgetify/': ['premium','budgetify_3999_lt'],
   'Dedupe/': ['Pro','com.curiouscreatorsco.Dedupe.pro.lifetime.notrial.39_99'],
-  'Wozi': ['wozi_pro_2023'],
-  'Documents': ['com.readdle.ReaddleDocsIPad.subscription.month10_bf22']
+  'Wozi': ['wozi_pro_2023']
 };
 
 // =========    固定部分  ========= // 
