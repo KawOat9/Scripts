6cb021658498bd828c6e5df2cd4fedca1cf14731
@KawOat9 <boonseub85@gmail.com>
2026-05-25 19:21:49 +0700

    Update BuyRevenueCatNotif2.js

--- BuyRevenueCatNotif2.js
+++ BuyRevenueCatNotif2.js
@@ -210,7 +210,7 @@ const mapping = {
   'Budgetify/': ['premium','budgetify_3999_lt'],
   'Dedupe/': ['Pro','com.curiouscreatorsco.Dedupe.pro.lifetime.notrial.39_99'],
   'Wozi': ['wozi_pro_2023'],
-  'Documents/': ['plus']
+  'Documents/': ['pro']
 };
 
 // =========  Notification Functions  ========= //
