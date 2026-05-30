fdc422e0c8cdb915b0de6d9f78fc4064a0454e90
@KawOat9 <boonseub85@gmail.com>
2026-05-30 09:43:31 +0700

    Update RevenueCat.ddm1023.js, Backup_Datas.txt and one other file

--- RevenueCat.ddm1023.js
+++ RevenueCat.ddm1023.js
@@ -401,7 +401,8 @@ const listua = {
   'CPUMonitor': { name: 'Pro', id: 'com.mars.cpumonitor_removeAd', cm: 'sjb' },  //手机硬件管家
   'totowallet': { name: 'all', id: 'com.ziheng.totowallet.onetimepurchase', cm: 'sjb' },  //图图记账
   '1Blocker': { name: 'premium', id: 'blocker.ios.iap.lifetime', cm: 'sjb' },  //1Blocker-广告拦截
-  'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' }  //VSCO-照片与视频编辑编辑
+  'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' },  //VSCO-照片与视频编辑编辑
+  'Fonts': { name: 'pro', id: 'com.wang.fonts.premium.yearly', cm: 'sja' }  //Fonts
 };
 
 // ... โค้ดเดิมด้านบน (รายชื่อใน listua) ...
