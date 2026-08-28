f3991d305d3afd52a00eb068af466087bc88e19b
@KawOat9 <boonseub85@gmail.com>
2026-08-29 01:40:59 +0700

    Update Carrot.js

--- Carrot.js
+++ Carrot.js
@@ -28,7 +28,7 @@ obj = {
     "result": {
       "serverDate": {
         "__type": "Date",
-        "iso": "2024-03-08T08:16:11.477Z"
+        "iso": "2026-03-08T08:16:11.477Z"
       },
       "subscriptions": [
         {
@@ -39,13 +39,13 @@ obj = {
           "appStartingVersion": "5.12.9.0",
           "receipt": "MIT",
           "productId": "com.grailr.carrotWeather.premiumFamily1year",
-          "purchaseTime": 1708674480000,
-          "originalPurchaseTime": 1708674481000,
-          "expirationTime": "1771852680000",
+          "purchaseTime": 1771832880000,
+          "originalPurchaseTime": 1771832880000,
+          "expirationTime": "1803368881000",
           "in_app_ownership_type": "PURCHASED",
           "expirationReason": null,
           "isInBillingRetryPeriod": null,
-          "autoRenewStatus": 0,
+          "autoRenewStatus": 1,
           "cancellationDate": null,
           "cancellationReason": null,
           "priceConsentStatus": null,
@@ -54,8 +54,8 @@ obj = {
           "status": 0,
           "service": "apple",
           "environment": "Production",
-          "createdAt": "2024-02-23T07:48:07.554Z",
-          "updatedAt": "2025-02-08T07:24:51.399Z",
+          "createdAt": "2026-02-23T07:48:07.554Z",
+          "updatedAt": "2026-02-08T07:24:51.399Z",
           "deviceId": [
             "WfjT0vx1hx",
             "pPsiN1AKdt"
@@ -63,7 +63,7 @@ obj = {
           "lastNotification": "DID_CHANGE_RENEWAL_STATUS",
           "lastNotificationDate": {
             "__type": "Date",
-            "iso": "2025-02-24T11:47:39.182Z"
+            "iso": "2026-02-24T11:47:39.182Z"
           },
           "objectId": "m5JpZFig4u",
           "__type": "Object",
